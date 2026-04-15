bf5d695cbd0fe27727629c0e4a3929244527e172
kgudiel-2025180 <kgudiel-2025180@kinal.edu.gt>
2026-04-15 11:38:43 -0600

    feat: se agregó la entidad de detalles de Venta

---
+++ src/main/java/com/kennygudiel/Kinalapp/entity/DetalleVenta.java
@@ -0,0 +1,122 @@
+package com.kennygudiel.Kinalapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "detalle_venta")
+public class DetalleVenta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo_detalle_venta")
+    private Integer idDetalle;
+
+    @Column
+    private Integer cantidad;
+
+    @Column(name = "precio_unitario")
+    private Double precio;
+
+    @Column
+    private Double subtotal;
+
+    @Column
+    private int estado;
+
+
+    // 🔹 MANY TO ONE -> VENTA
+    @ManyToOne
+    @JoinColumn(name = "ventas_codigo_venta")
+    private Venta venta;
+
+
+    // 🔹 MANY TO ONE -> PRODUCTO
+    @ManyToOne
+    @JoinColumn(name = "productos_codigo_producto")
+    private Producto producto;
+
+
+    public DetalleVenta() {
+    }
+
+
+    public DetalleVenta(Integer idDetalle,
+                        Integer cantidad,
+                        Double precio,
+                        Double subtotal,
+                        int estado,
+                        Venta venta,
+                        Producto producto) {
+
+        this.idDetalle = idDetalle;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.subtotal = subtotal;
+        this.estado = estado;
+        this.venta = venta;
+        this.producto = producto;
+    }
+
+
+    public Integer getIdDetalle() {
+        return idDetalle;
+    }
+
+    public void setIdDetalle(Integer idDetalle) {
+        this.idDetalle = idDetalle;
+    }
+
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
+
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
